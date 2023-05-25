e9fda7303dafb4abaea4a0f15995856491d09f9f
shraddhagaudani <shraddhagaudani@gmail.com>
2023-05-24 23:35:10 -0700

    25/5/2023

--- lib/views/screens/homepage.dart
+++ lib/views/screens/homepage.dart
@@ -25,7 +25,6 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
